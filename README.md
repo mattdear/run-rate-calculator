1d5cfa04d00b621497c0423397f56c1d7ffecc76
Matt Dear <mrmdear@gmail.com>
2019-07-13 16:01:47 +0100

    Completed and tested version 1.6

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # run_rate_calculator
 
 ## About
-The cricket run rate calculator is made using python. Version 1.5 can be used for the first innings and second innings of a game however, it cannot be used for multi-day game formats. Once each calculation is completed you will be asked if you would like to update the run rate, you will then be asked for the required information and the calculation will be reapeated. Once the innings is completed respond 'n' to the update request and the application will close. If you would then like to use it for the second innings you will need to run the program again.  
+The cricket run rate calculator is made using python. Version 1.6 can be used for the first innings and second innings of a game however, it cannot be used for multi-day game formats. Once each calculation is completed you will be asked if you would like to update the run rate, you will then be asked for the required information and the calculation will be reapeated. Once the innings is completed respond 'n' to the update request and the application will close. If you would then like to use it for the second innings you will need to run the program again. 
 
 ## How to run
 1. Clone this repository https://github.com/mattdear/run_rate_calculator.git
@@ -9,8 +9,10 @@ The cricket run rate calculator is made using python. Version 1.5 can be used fo
 3. Type ./runRate.py in BASH or in CMD to start the application.
 4. To calculate the run rate again you will need to repeat step 3.
 
+## New in 1.6
+- Added try catch to all user inputs.
+
 ## Future updates
 - Add starting second innings without restarting the application.
 - Add support for multi-day formats. 
 - Add a GUI interface.
-- Add try catch to user input.
--- runRate.py
+++ runRate.py
@@ -1,22 +1,82 @@
 #!/bin/python
 
 #Run Rate Calculator by Matthew Dear
-#Version 1.5
+#Version 1.6
 
 firstInningsTotal = 0.00
 inGame = "y"
+
 print("\nWelcome to the run rate calculator... by Matthew Dear")
-isFirstInnings = str(input("First innings? (y/n) "))
-gameLength = int(input("Game length? (20/50) "))
+
+while True:
+    try:
+        isFirstInnings = str.lower((input("First innings? (y/n) ")))
+    except ValueError:
+        print("Invalid please try again.")
+
+    if isFirstInnings not in ('y','n'):
+        print("Invalid please try again.")
+    else:
+        break        
+
+gameLength = 0
+
+while True:
+    try:
+        gameLength = int(input("Game length? (20/50) "))
+    except ValueError:
+        print("Invalid please try again.")
+    
+    if gameLength == 20:
+        break
+    elif gameLength == 50:
+        break    
+    else:
+        print("Invalid please try again.")
 
 if isFirstInnings == "y":
 
     while inGame == "y":
-        currentRuns = float(input("\nCurrent run total? "))
-        oversUsed = float(input("Overs used? "))
+
+        currentRuns = 0.00
+
+        while True:
+            try:
+                currentRuns = float(input("\nCurrent run total? "))
+            except ValueError:
+                print("Invalid please try again.")
+
+            if currentRuns <= 0:
+                print("Invalid please try again.")
+            else:
+                break
+
+        oversUsed = 0.00
+
+        while True:
+            try:        
+                oversUsed = float(input("Overs used? "))
+            except ValueError:
+                print("Invalid please try again.")
+
+            if oversUsed <= 0:
+                print("Invalid please try again.")
+            else:
+                break
+
         runRate = currentRuns / oversUsed
         print("\nCurrent run rate ", "%.2f" % runRate, "\n")
-        inGame = input("update? (y/n) ")
+        
+        while True:
+            try:
+                inGame = str.lower(input("update? (y/n) "))
+            except ValueError:
+                print("Invalid please try again.")
+
+            if inGame not in ('y','n'):
+                print("Invalid please try again.")
+            else:
+                break        
 
     else:
         print("\n\nThanks for using the run rate calculator... by Matthew Dear\n")
@@ -26,16 +86,61 @@ else:
     while inGame == "y":
 
         if firstInningsTotal == 0.00:
-            firstInningsTotal = float(input("\n1st innings run total? "))
+
+            while True:
+                try:
+                    firstInningsTotal = float(input("\n1st innings run total? "))
+                except ValueError:
+                     print("Invalid please try again.")
+
+                if firstInningsTotal <= 0:
+                    print("Invalid please try again.")
+                else:
+                    break                
 
         else:
-            currentRuns = float(input("Current run total? "))
-            oversUsed = float(input("Overs used? "))
+
+            currentRuns = 0.00
+
+            while True:
+                try:
+                    currentRuns = float(input("Current run total? "))
+                except ValueError:
+                    print("Invalid please try again.")
+
+                if currentRuns <= 0:
+                    print("Invalid please try again.")
+                else:
+                    break
+
+            oversUsed = 0.00    
+
+            while True:
+                try:        
+                    oversUsed = float(input("Overs used? "))
+                except ValueError:
+                    print("Invalid please try again.")
+
+                if oversUsed <= 0:
+                    print("Invalid please try again.")
+                else:
+                    break
+
             runDifferance = (firstInningsTotal - currentRuns) + 1
             oversRemaining = gameLength - oversUsed
             runRate = runDifferance / oversRemaining
             print("\nRequired run rate ", "%.2f" % runRate, "\n")
-            inGame = input("update? (y/n) ")
+            
+            while True:
+                try:
+                    inGame = str.lower(input("update? (y/n) "))
+                except ValueError:
+                    print("Invalid please try again.")
+
+                if inGame not in ('y','n'):
+                    print("Invalid please try again.")
+                else:
+                    break        
 
     else:
         print("\n\nThanks for using the run rate calculator... by Matthew Dear\n")
