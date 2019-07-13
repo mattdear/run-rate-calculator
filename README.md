8533b1265b78af280e05e33cfd301b08e8c89b61
Matt Dear <mrmdear@gmail.com>
2019-07-13 14:21:42 +0100

    Completed and tested version 1.5

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # run_rate_calculator
 
 ## About
-This is a cricket run rate calculator made in python. Version 1.0 can only be used once the first innings is completed. You will have to enter all information every time you want to update the required run rate. I have plans to extend the application with more features in the future.  
+The cricket run rate calculator is made using python. Version 1.5 can be used for the first innings and second innings of a game however, it cannot be used for multi-day game formats. Once each calculation is completed you will be asked if you would like to update the run rate, you will then be asked for the required information and the calculation will be reapeated. Once the innings is completed respond 'n' to the update request and the application will close. If you would then like to use it for the second innings you will need to run the program again.  
 
 ## How to run
 1. Clone this repository https://github.com/mattdear/run_rate_calculator.git
 2. Using the BASH or CMD to move to the directory that you have cloned the repository to.
 3. Type ./runRate.py in BASH or in CMD to start the application.
 4. To calculate the run rate again you will need to repeat step 3.
+
+## Future updates
+- Add starting second innings without restarting the application.
+- Add support for multi-day formats. 
+- Add a GUI interface.
+- Add try catch to user input.
--- runRate.py
+++ runRate.py
@@ -1,17 +1,41 @@
 #!/bin/python
 
-remainingBalls = 0.00
-runRate = 0.00
-print " "
-print("Run rate calculator...")
-firstInnings = float(input("1st innings total runs? "))
-currentRuns = float(input("2nd innings current run? "))
-remainingOvers = float(input("Overs remaining? "))
+#Run Rate Calculator by Matthew Dear
+#Version 1.5
 
-runDifferance = (firstInnings - currentRuns)
-runDifferance = runDifferance + 1
-remainingBalls = remainingOvers * 6
-runrate = runDifferance / remainingBalls
-print " "
-print "The required run rate is","%.2f" % runrate
-print " "
+firstInningsTotal = 0.00
+inGame = "y"
+print("\nWelcome to the run rate calculator... by Matthew Dear")
+isFirstInnings = str(input("First innings? (y/n) "))
+gameLength = int(input("Game length? (20/50) "))
+
+if isFirstInnings == "y":
+
+    while inGame == "y":
+        currentRuns = float(input("\nCurrent run total? "))
+        oversUsed = float(input("Overs used? "))
+        runRate = currentRuns / oversUsed
+        print("\nCurrent run rate ", "%.2f" % runRate, "\n")
+        inGame = input("update? (y/n) ")
+
+    else:
+        print("\n\nThanks for using the run rate calculator... by Matthew Dear\n")
+
+else:
+
+    while inGame == "y":
+
+        if firstInningsTotal == 0.00:
+            firstInningsTotal = float(input("\n1st innings run total? "))
+
+        else:
+            currentRuns = float(input("Current run total? "))
+            oversUsed = float(input("Overs used? "))
+            runDifferance = (firstInningsTotal - currentRuns) + 1
+            oversRemaining = gameLength - oversUsed
+            runRate = runDifferance / oversRemaining
+            print("\nRequired run rate ", "%.2f" % runRate, "\n")
+            inGame = input("update? (y/n) ")
+
+    else:
+        print("\n\nThanks for using the run rate calculator... by Matthew Dear\n")
